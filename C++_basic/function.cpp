94d5bc568e219906ea9a841428e2b5927de8c96b
iamanujpratapsingh <anujpratapsingh567@gmail.com>
2026-03-05 16:18:12 +0530

    done var & identifiers

---
+++ C++_basic/function.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"anuj";
+    return 0;
+}
+int anuj(){
+
+}
---
+++ C++_variable_constant/variable.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int score;
+
+    int anuj_balance;
+
+    anuj_balance=1000;
+
+    int balance=500;
+    //constant variable is a variable whose value cannot be changed once it is assigned. It is declared using the keyword 'const'.
+    const int uid=1234;
+
+    
+    return 0;
+}
