93e1a12c7d8a712e4de1127d3098dedffe9fc961
iamanujpratapsingh <anujpratapsingh567@gmail.com>
2026-03-05 19:24:49 +0530

    make new folder

---
+++ C++_variable_constant/swarnika.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //constant variable is a variable whose value cannot be changed once it is assigned. It is declared using the keyword 'const'.
+    const int uid=1234;
+
+    //uid=5678; //error: assignment of read-only variable 'uid'
+    
+    return 0;
+}
