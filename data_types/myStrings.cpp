fc6090f10897af99d5300ff5cbd7352590c36bc2
iamanujpratapsingh <anujpratapsingh567@gmail.com>
2026-03-07 01:12:33 +0530

    commplete till myStrings

---
+++ data_types/myStrings.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string favouriteTea = "Lemon Tea \n";
+    string favouriteTeaSpace = "Lemon Tea \t";
+
+    string description = "known as the \"best\" tea";
+
+    cout << favouriteTea << description << endl;
+    
+    cout << favouriteTeaSpace << description << endl;
+    
+    return 0;
+}
