e8b42472bc6573dedc5721b6e49facbcead7dcbb
iamanujpratapsingh <anujpratapsingh567@gmail.com>
2026-03-07 00:29:51 +0530

    complete till data types

---
+++ data_types/data.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using std::cout;
+using std::endl;
+using namespace std;
+int main(){
+
+    int teaLeaves=50;
+    float waterTemp=85.3;
+    double priceOfTea=299.9;
+    char teaGrade='A';
+    bool isTeaReady = false;
+    cout <<isTeaReady<<endl;
+
+    return 0;
+}
