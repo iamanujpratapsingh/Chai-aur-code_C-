0f9e31ea402a5fe63130524b53f8a32c3eafc16d
iamanujpratapsingh <anujpratapsingh567@gmail.com>
2026-03-07 00:45:48 +0530

    complete till modifiers

---
+++ data_types/modifiers.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int smallTeaPack = 100;
+    unsigned smallTeaPacked = 200;
+    long largeTeaStorage =1000000000;
+    unsigned long largeStorage = -49000000;
+    unsigned long long storageKaBaap = 399999999999999;
+    short teaSample = 5;
+    __int16 precise_bits = 3;
+    cout<< storageKaBaap<<endl;
+    cout<< precise_bits <<endl;
+    return 0;
+}
